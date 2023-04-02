a9645158eacf1a25f3459c08295bd5399cbe3fb7
Lavein18 <111446580+Lavein18@users.noreply.github.com>
2023-04-02 23:55:45 +0530

    Create Exp no 10.java

---
+++ Exp no 10.java
@@ -0,0 +1,29 @@
+package com.journaldev.model;
+
+public class Customer {
+
+	private int Orderid;
+	private String Customername;
+	private int Orderdate;
+	public Customer() {
+	}
+	public int getOrderId() {
+		return Orderid;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
